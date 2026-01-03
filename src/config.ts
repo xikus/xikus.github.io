af6ce438a1d8c85e6e7fb2ab757e3ad82679b37f
hugos <xikus2023@outlook.com>
2026-01-03 23:36:21 +0800

    [fix][config]: 启用banner并更新图片路径为coast.jpg [fix][config]: 启用banner图片的信用文本显示

--- src/config.ts
+++ src/config.ts
@@ -16,11 +16,11 @@ export const siteConfig: SiteConfig = {
 		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
-		enable: false,
-		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		enable: true,
+		src: "assets/images/coast.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
-			enable: false, // Display the credit text of the banner image
+			enable: true, // Display the credit text of the banner image
 			text: "不像海边的海边", // Credit text to be displayed
 			url: "", // (Optional) URL link to the original artwork or artist's page
 		},
